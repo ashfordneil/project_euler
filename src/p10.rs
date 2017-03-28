dbf00ba1ebe833b74951323a3610b1592526cb6f
Neil Ashford <ashfordneil0@gmail.com>
2017-03-28 11:29:54 +1000

    problem 10 complete

---
+++ src/p10.rs
@@ -0,0 +1,33 @@
+//! The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+//! Find the sum of all the primes below two million.
+
+// stolen from p7
+/// An iteartor that generates sequential prime numbers.
+struct PrimeGenerator {
+    found: Vec<u64>,
+}
+
+impl Iterator for PrimeGenerator {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<u64> {
+        let prime = match self.found.last() {
+            // base case
+            None => 2,
+            Some(start) => {
+                (*start..)
+                    .filter(|&number| self.found.iter().all(|divisor| number % divisor > 0))
+                    .next()
+                    .unwrap()
+            }
+        };
+        self.found.push(prime);
+        Some(prime)
+    }
+}
+
+fn main() {
+    let answer: u64 = PrimeGenerator { found: Vec::new() }.take_while(|&x| x < 2_000_000).sum();
+    println!("Answer: {}", answer);
+}
